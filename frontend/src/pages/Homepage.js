93482447b24c72cebad4d19b8d0cb68f16ae3878
Mohamed Marwane Bellagha <mohamed-marwane.bellagha@laplateforme.io>
2022-11-21 19:23:17 +0100

    fix

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -3,20 +3,20 @@ import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
 
 export default function Homepage() {
-  const { loading, error, data } = useFetch('http://localhost:1337/api/videos')
+  const { loading, error, data } = useFetch('http://localhost:1337/api/videos?populate=*')
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
-
-  console.log(data.data[0].attributes.title_video)
-  data.data.map(video => (console.log(video)))
+  console.log(data.data[0].attributes.image.data.attributes.url)
 
   return (
     <div>
       {data.data.map(video => (
+        
         <div key={video.id} className="review-card">
           <h2>{video.attributes.title_video}</h2>
           <p>{video.attributes.description_video}</p>
+          <img src={'http://localhost:1337'+video.attributes.image.data.attributes.url}></img>
           <small>console list</small>
           <Link to={`/details/${video.id}`}>Read more</Link>
         </div>
--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import useFetch from '../hooks/useFetch'
+
 
 export default function ReviewDetails() {
+  const { id } = useParams()
+  const { loading, error, data } = useFetch('http://localhost:1337/api/videos/' + id)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  console.log(data.data.attributes)
+
   return (
     <div>
-      review details
+        <h2>{data.data.attributes.title_video}</h2>
+        <p>{data.data.attributes.description_video}</p>
+       <ReactPlayer url={data.data.attributes.url_video}></ReactPlayer>
+        review details
     </div>
   )
 }
